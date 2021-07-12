355d95c3f227ac0ab331403b7e37cc7945f18c66
ivanCanaveral <canaveral.ivan@gmail.com>
2021-07-13 00:43:01 +0200

    cnn-gru started

---
+++ cnn-gru/setup.sh
@@ -0,0 +1,6 @@
+# install tensorflow docs
+pip install -q git+https://github.com/tensorflow/docs
+
+# download the data
+wget -q https://git.io/JGc31 -O ucf101_top5.tar.gz
+tar xf ucf101_top5.tar.gz
